9fc781a1ecbf5a590c992e264f14db4eaba8bd63
Steampunk Destroyer <161687403+andrei5713@users.noreply.github.com>
2025-02-19 20:11:53 +0800

    andrei

--- routes/scrum.php
+++ routes/scrum.php
@@ -3,11 +3,17 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ScrumController;
 
-Route::controller(ScrumController::class)->group(function(){
-    Route::middleware('auth')->group(function(){
+Route::controller(ScrumController::class)->group(function () {
+    Route::middleware('auth')->group(function () {
         Route::get('/scrum/dashboard', 'getDashboardDatas')->name('scrum-dashboard');
         Route::get('/scrum/board', 'getBoardDatas')->name('scrum-board');
         Route::get('/scrum/timeline', 'getTimelineDatas')->name('scrum-timeline');
         Route::get('/scrum/backlog', 'getBacklogDatas')->name('scrum-backlog');
     });
 });
+
+Route::get('/scrum/upgrade-plan', function () {
+    return inertia('Scrum/UpgradePlan', [
+        'title' => 'Upgrade Plan',
+    ]);
+})->name('upgrade-plan');
