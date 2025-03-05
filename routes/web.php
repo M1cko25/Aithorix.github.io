70ab742605d3669e667233f26a35073ac87b0c9e
M1cko25 <154508740+M1cko25@users.noreply.github.com>
2025-03-06 02:47:10 +0800

    meeting summary ui update

--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,5 @@ use App\Http\Controllers\MeetingConferenceController;
 Route::inertia('/', 'Landing')->name('landing');
 Route::get('/home', [HomeController::class, 'getUserDatas'])->name('home')->middleware('auth');
 Route::inertia('/meeting/home', 'Meeting/MeetingHome')->name('meeting-home')->middleware('auth');
+Route::inertia('/meetings', 'Meeting/MeetingSummaries')->name('meetings')->middleware('auth');
 Route::inertia('/meeting/conference', 'Meeting/Conference')->name('meeting-conference')->middleware('auth');
