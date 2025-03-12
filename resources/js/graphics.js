122ff9760557b22d49ff95ca669ea8c9ab8353bc
M1cko25 <154508740+M1cko25@users.noreply.github.com>
2025-03-12 22:54:09 +0800

    assets added

--- resources/js/graphics.js
+++ resources/js/graphics.js
@@ -1,10 +1,10 @@
 export default {
-    educationIllustration: new URL('../../../public/assets/education-illustration.svg', import.meta.url).href,
-    eventIllustration: new URL('../../../public/assets/eventPlanning-illustration.svg', import.meta.url).href,
-    scrumIllustration: new URL('../../../public/assets/scrum-illustration.svg', import.meta.url).href,
-    researchIllustration: new URL('../../../public/assets/research-illustration.svg', import.meta.url).href,
-    contentIllustration: new URL('../../../public/assets/content-illustration.svg', import.meta.url).href,
-    signinIllustration: new URL('../../../public/assets/signin-illustration.svg', import.meta.url).href,
-    signupIllustration: new URL('../../../public/assets/signup-illustration.svg', import.meta.url).href,
-    noDataIllustration: new URL('../../../public/assets/noDataIllustration.svg', import.meta.url).href,
+    educationIllustration: new URL('@/assets/education-illustration.svg', import.meta.url).href,
+    eventIllustration: new URL('@/assets/eventPlanning-illustration.svg', import.meta.url).href,
+    scrumIllustration: new URL('@/assets/scrum-illustration.svg', import.meta.url).href,
+    researchIllustration: new URL('@/assets/research-illustration.svg', import.meta.url).href,
+    contentIllustration: new URL('@/assets/content-illustration.svg', import.meta.url).href,
+    signinIllustration: new URL('@/assets/signin-illustration.svg', import.meta.url).href,
+    signupIllustration: new URL('@/assets/signup-illustration.svg', import.meta.url).href,
+    noDataIllustration: new URL('@/assets/noDataIllustration.svg', import.meta.url).href,
 }
